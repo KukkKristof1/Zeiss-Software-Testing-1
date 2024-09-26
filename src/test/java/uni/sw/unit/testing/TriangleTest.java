df078fbf69b40c6e0f96038ce89b6c91feae0230
Kristóf  Kukk <kukk@iit.uni-miskolc.hu>
2024-09-26 12:36:24 +0200

    Fifth unit test

--- src/test/java/uni/sw/unit/testing/TriangleTest.java
+++ src/test/java/uni/sw/unit/testing/TriangleTest.java
@@ -39,4 +39,23 @@ public class TriangleTest {
 
     }
 
+    @Test
+    public void testRightAngeledTriangle()
+    {
+        Triangle triangle = new Triangle(6, 8, 10);
+
+        assertTrue(triangle instanceof Triangle);   
+
+        assertTrue(triangle.isRightAngeled());
+    }
+
+    @Test
+    public void testPerimeter()
+    {
+        Triangle triangle = new Triangle(100, 251, 178);
+
+        assertTrue(triangle instanceof Triangle);
+
+        assertEquals(triangle.getPerimeter(), 529);
+    }
 }
