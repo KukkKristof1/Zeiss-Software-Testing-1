bd39abd5a032fbbd820be1a7e668013a57200744
KukkKristof1 <kristof.kukk@student.uni-miskolc.hu>
2024-11-07 12:25:34 +0100

    Mocking updated

--- src/test/java/uni/sw/unit/testing/UserTest.java
+++ src/test/java/uni/sw/unit/testing/UserTest.java
@@ -1,17 +1,16 @@
 package uni.sw.unit.testing;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;  
 
 public class UserTest {
     
     @Test
+    @DisplayName("Default constructor test")
     public void testDefaultConstructor(){
         User user = new User();
         assertNotNull(user);
@@ -20,6 +19,27 @@ public class UserTest {
         assertNull(user.getPassword());
         assertEquals(user.getId(), 0);
         assertThrows(UnsupportedOperationException.class, user::isLoggedIn);
+    }
+
+    @Test
+    @DisplayName("Mock test")
+    public void testMocking(){
+        User user = mock(User.class);
+        when(user.getUserName()).thenReturn("Edina");
+        when(user.getPassword()).thenReturn("korikedinajelszo");
+        when(user.getId()).thenReturn(123);
+        when(user.isLoggedIn()).thenReturn(true);
+
+        assertEquals(user.getUserName(),  "Edina", "Username should be 'Edina'");
+        assertEquals(user.getPassword(), "korikedinajelszo", "Password should be 'korikedinajelsoz'");
+        assertEquals(user.getId(), 123, "Id should be 123");
+        assertTrue(user.isLoggedIn(), "Logged in should be true");
+
+        verify(user).getUserName();
+        verify(user).getPassword();
+        verify(user).getId();
+        verify(user).isLoggedIn();
+
     }
 
 }
