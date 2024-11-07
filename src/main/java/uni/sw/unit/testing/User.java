b9786275c99343f94fe6bee0836d07955654a084
KukkKristof1 <kristof.kukk@student.uni-miskolc.hu>
2024-11-07 13:34:47 +0100

    New functions and mocking tests for the User class

--- src/main/java/uni/sw/unit/testing/User.java
+++ src/main/java/uni/sw/unit/testing/User.java
@@ -4,11 +4,14 @@ public class User {
     private int _id;
     private String _userName;
     private String _password;
-    private double _academicAverage;
+
 
     public String getUserName() { return _userName; }
     public String getPassword() { return _password; }
     public int getId() { return _id; }
+    public double getAcademicAverage(int semester) { return 0; }
+
+    public boolean update(String newPassword, boolean isLoggedInValue) { return false; }
 
     boolean isLoggedIn() { throw new UnsupportedOperationException("not implemented"); }
 
--- src/test/java/uni/sw/unit/testing/UserTest.java
+++ src/test/java/uni/sw/unit/testing/UserTest.java
@@ -13,7 +13,7 @@ public class UserTest {
     @DisplayName("Default constructor test")
     public void testDefaultConstructor(){
         User user = new User();
-        assertNotNull(user);
+        //assertNotNull(user);
 
         assertNull(user.getUserName());
         assertNull(user.getPassword());
@@ -25,14 +25,15 @@ public class UserTest {
     @DisplayName("Mock test")
     public void testMocking(){
         User user = mock(User.class);
+
         when(user.getUserName()).thenReturn("Edina");
         when(user.getPassword()).thenReturn("korikedinajelszo");
         when(user.getId()).thenReturn(123);
         when(user.isLoggedIn()).thenReturn(true);
 
-        assertEquals(user.getUserName(),  "Edina", "Username should be 'Edina'");
-        assertEquals(user.getPassword(), "korikedinajelszo", "Password should be 'korikedinajelsoz'");
-        assertEquals(user.getId(), 123, "Id should be 123");
+        assertEquals( "Edina", user.getUserName(), "Username should be 'Edina'");
+        assertEquals("korikedinajelszo", user.getPassword(), "Password should be 'korikedinajelsoz'");
+        assertEquals(123, user.getId(), "Id should be 123");
         assertTrue(user.isLoggedIn(), "Logged in should be true");
 
         verify(user).getUserName();
@@ -40,6 +41,26 @@ public class UserTest {
         verify(user).getId();
         verify(user).isLoggedIn();
 
-    }
+        assertEquals(16, user.getPassword().length());
+        verify(user, times(2)).getPassword();
 
+        doThrow(new UnsupportedOperationException()).when(user).isLoggedIn();
+        assertThrows(UnsupportedOperationException.class, user::isLoggedIn, "Method should throw UnsupportedOperationException");
+
+        when(user.getAcademicAverage(anyInt())).thenReturn(4.6);
+        assertEquals(4.6, user.getAcademicAverage(2));
+        assertEquals(4.6, user.getAcademicAverage(3));
+
+        when(user.update(anyString(), eq(true))).thenReturn(true);
+        when(user.update(anyString(), eq(false))).thenReturn(false);
+        
+        assertFalse(user.update("newPass", false), "Update function should return false (not logged in)");
+        assertTrue(user.update("newPass", true), "Update function should return true (logged in)");
+
+        doThrow(new UnsupportedOperationException()).when(user).update(anyString(), anyBoolean());
+        assertThrows(UnsupportedOperationException.class,
+        () -> {user.update("", false);},
+        "Method should throw UnsupportedOperationException");
+
+    }
 }
